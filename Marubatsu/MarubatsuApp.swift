07e351687845aae5378307b75dfa013033c29069
zonehisa <nijipapazoneokun123@gmail.com>
2025-02-15 08:34:51 +0900

    Initial Commit

---
+++ Marubatsu/MarubatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuApp.swift
+//  Marubatsu
+//
+//  Created by 佐藤幸久 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MarubatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
